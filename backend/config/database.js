0c4d2f069964b29904a7429393d5b81e63e9b1d4
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-12-27 10:44:34 +0100

    Created User model

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/FitHub');
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log('MongoDB Connected:', conn.connection.host);
         console.log('Database:', conn.connection.name);
